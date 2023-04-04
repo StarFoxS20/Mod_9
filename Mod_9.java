ef6c6997349664830065bbd81d4e695c6d19951f
StarFoxS20 <127164017+StarFoxS20@users.noreply.github.com>
2023-04-03 22:23:09 -0300

    Mod_9

---
+++ Mod_9.java
@@ -0,0 +1,18 @@
+public class Mod_9 {
+
+
+    public static void main(String args[])
+    {
+
+//Variavel declarada no tipo primitivo
+
+        int num = 9;
+        System.out.println(num);
+
+//convertida para tipo wrapper
+
+        Integer num1 = 18;
+        System.out.println(num1);
+
+    }
+}
